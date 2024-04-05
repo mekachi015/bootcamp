39d319f2de77fc3c8e32bcf0d1743c3628788299
mekachi015 <abielmakoti@gmail.com>
2024-04-05 10:26:11 +0200

    Functions

---
+++ javascript/functions.js
@@ -0,0 +1,51 @@
+const prompt = require('prompt-sync')();
+
+let a = parseFloat(prompt("Enter number 1:"));
+
+let b = parseFloat(prompt('Enter number 2:'));
+
+let operator = prompt('Select either (+ - * /) opertator');
+
+function calcSum(a,b){
+    let string = "The final answer is:";
+    return string+ " " + a+b;
+}
+
+function calMultiple(a,b){
+    let string = "The final answer is:";
+    return string+ " " + a*b;
+}
+
+
+function calDivision(a,b){
+    let string = "The final answer is:";
+    return string+ " " + a/b;
+}
+
+function calMinus (a,b){
+    let string = "The final answer is:";
+    return string+ " " + a-b;
+}
+ let result = " ";
+
+switch (operator){
+    case "*":
+    result = calMultiple(a,b);
+    break;
+
+    case "-":
+    result = calMinus(a,b);
+    break;
+
+    case "/":
+    result = calDivision(a,b);
+    break;
+
+    case "+":
+    result = calcSum(a,b);
+    break;
+
+    default:
+    result = "You chose the incorrect operator";
+}
+console.log(result);
