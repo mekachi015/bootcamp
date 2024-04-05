cabcd080ffbf3eed427642953b8d1aa5f375d4ac
mekachi015 <abielmakoti@gmail.com>
2024-04-05 10:45:13 +0200

    Functions part 2

---
+++ javascript/functions2.js
@@ -0,0 +1,14 @@
+var x = 150;
+var b =  Math.floor(Math.random() * 100) + 1;
+
+function myFun(){
+    y = x*2;
+
+    n = b * 150;
+    console.log("The function multiplies by 2 and the answer is:" + y);
+    console.log("Multiplying the value of b by 150: " + n);
+}
+
+myFun();
+console.log("the original value of x: " + x);
+console.log("the original value of b = " + b);
