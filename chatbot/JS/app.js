619b69ac93819d2d42a0c494d6dd251668abc1b6
mekachi015 <abielmakoti@gmail.com>
2024-04-09 16:25:06 +0200

    chatbot commit

---
+++ chatbot/JS/app.js
@@ -0,0 +1,26 @@
+function talk(){
+//Think about key : value
+
+    var know = {
+        "Hello" : "Hi there &#128540", //check hexahtml - emoji to hexadecimal
+        "Who are you?" : "My name is Alice",
+        "How are you?" : "I am good :-)",
+        "What can i do for you" : "Give us a follow and like!",
+        "Your followers?" : "I have a 1000 more than you do :-)",
+        "Okay" : "Thanks for the conversation",
+        "Bye" :"Okay, Meet again soon, nice chattiing to you"
+    }
+
+    var user =document.getElementById('userBox').ariaValueMax; ///get user input
+
+    document.getElementById('ChatLog').innerHTML = user + "<br>"
+
+    if (user in know)
+    {
+        document.getElementById('ChatLog').innerHTML = know[userBox] + "<br>"
+    }
+
+    else {
+        document.getElementById('ChatLog').innerHTML = "Sorry I dont  understand what you mean <br>"
+    }
+}
